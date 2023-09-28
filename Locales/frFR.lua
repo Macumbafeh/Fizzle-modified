6234aab5ab2bf3d2a052ff632176599f43a83eeb
Macumba <stephanjeff@hotmail.fr>
2023-09-28 07:07:45 +0200

    added fr locale and some fix for char frame

---
+++ Locales/frFR.lua
@@ -0,0 +1,22 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Fizzle", "frFR", true)
+if not L then return end
+
+-- Core.lua
+L["Displays Item Quality and Durability in the character frame."] = "Affiche la qualité et la Durabilité de l'équipement dans le cadre d'information du personnage."
+L["Percent"] = "Pourcentage"
+L["Toggle percentage display."] = "Active l'affichage des pourcentages."
+L["Border"] = "Bordure"
+L["Toggle quality borders."] = "Active les bordures de qualité."
+L["Invert"] = "Inverser"
+L["Show numbers the other way around. Eg. 0% = full durability , 100 = no durability."] = "Montre les chiffres dans l'autre sens. ex : 0% = Durabilité complète, 100% = plus de durabilité."
+L["Hide Text"] = "Cacher le texte"
+L["Hide durability text."] = "Cache le texte de la Durabilité."
+L["Show Full"] = "Montrer quand pleine"
+L["Show durability when full."] = "Montrer la durabulité quand elle est pleine."
+L["Show Repair Cost"] = "Afficher le Coût des Réparations"
+L["Show the total repair cost of your items."] = "Affiche le montant total du coût des Réparations de vos objets."
+L["Discount"] = "Réduction"
+L["Show the repair cost minus the 10% faction discount."] = "Affichez le coût de réparation moins la réduction de faction de 10 %."
+-- Inspect.lua
+L["Inspect"] = "Inspection"
+L["Show item quality when inspecting people."] = "Affiche la qualité des objets quand on inspecte quelqu'un."
